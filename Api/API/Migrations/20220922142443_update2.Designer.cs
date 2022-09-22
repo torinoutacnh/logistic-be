56c83a0805957b7623c8b46c066c9ec2d0c07535
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2022-09-22 21:26:02 +0700

    migration

--- Api/API/Migrations/20220922142443_update2.Designer.cs
+++ Api/API/Migrations/20220922142443_update2.Designer.cs
@@ -4,6 +4,7 @@ using FU.Repository.DbStore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(Store))]
-    partial class StoreModelSnapshot : ModelSnapshot
+    [Migration("20220922142443_update2")]
+    partial class update2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api/API/Migrations/20220922142443_update2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class update2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Routes_StopPoints_FromId",
+                table: "Routes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Routes_StopPoints_ToId",
+                table: "Routes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Routes_FromId",
+                table: "Routes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Routes_ToId",
+                table: "Routes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_FromId",
+                table: "Routes",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_ToId",
+                table: "Routes",
+                column: "ToId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Routes_StopPoints_FromId",
+                table: "Routes",
+                column: "FromId",
+                principalTable: "StopPoints",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Routes_StopPoints_ToId",
+                table: "Routes",
+                column: "ToId",
+                principalTable: "StopPoints",
+                principalColumn: "Id");
+        }
+    }
+}
