04d656bbe64f61d556b96f259adb1722f87e57c9
zZZuyZz <duylieng.ntt@gmail.com>
2022-09-22 16:16:34 +0700

    a

--- Api/API/Utils/Constant/MessageConstant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Utils.Constant
-{
-    public class MessageConstant
-    {
-        public const string NotFound = "Not found!";
-        public const string Success = "Success !";
-    }
-}
