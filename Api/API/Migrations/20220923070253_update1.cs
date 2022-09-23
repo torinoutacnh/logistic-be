cfa805f44ddaec785f47d552098c3154f94618d9
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2022-09-23 14:03:49 +0700

    a

---
+++ Api/API/Migrations/20220923070253_update1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_CarNumber",
+                table: "Cars");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CarNumber",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CarNumber",
+                table: "Cars",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_CarNumber",
+                table: "Cars",
+                column: "CarNumber",
+                unique: true);
+        }
+    }
+}
